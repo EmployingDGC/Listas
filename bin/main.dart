c4a4f2fd2b2a21e080de8694166d9538217b31d5
EmployingDGC <63275906+EmployingDGC@users.noreply.github.com>
2020-04-26 00:36:14 -0300

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,28 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+  List<String> nomes = ['Davi', 'Mayane', 'Jackie'];
+
+  int tam = nomes.length; // tamanho da lista
+
+  nomes.add('Pretinha'); // adicionar um novo elemento
+
+  nomes.removeAt(2); // remover pelo index
+
+  nomes.insert(1, 'Thiago'); // adiciona em uma posição
+
+  print(nomes.contains('Davi')); // verifica se tem o valor na lista
+
+  List<Pessoa> pessoas = new List(); // criando uma lista de objetos
+
+  pessoas.add(new Pessoa()); // adicionando um novo objeto
+
+  /*
+   * Iterando lista de objetos
+  */
+  for (Pessoa p in pessoas) {
+    print(p);
+  }
+}
+
+class Pessoa {
+
 }
